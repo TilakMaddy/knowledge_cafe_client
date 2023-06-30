1a25aff5cfb59cd98be3658a8d6d4464c647423e
TilakMaddy <tilak.madichetti@gmail.com>
2023-06-30 20:53:35 +0530

    alpha tested

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const URL = process.env.NODE_ENV === 'production' ? 'https://knowledgecafeserver-production.up.railway.app/' : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' ? 'https://knowledge-cafe-cc-production.up.railway.app/' : 'http://localhost:3000';
 
 export const socket = io(URL, { transports: ['websocket', 'polling', 'flashsocket'] })
